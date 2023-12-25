727c09a523030b4857ee5aee486ef6352c8e2ffd
Yuri Watanabe <yuriwatanabe9090@gmail.com>
2023-12-25 17:45:45 -0300

    :tada: Starting project

---
+++ src/main/java/dev/takae/api/cms/TakaApiCmsApplication.java
@@ -0,0 +1,13 @@
+package dev.takae.api.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakaApiCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakaApiCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/takae/api/cms/TakaApiCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.takae.api.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakaApiCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
